1eafaf6cb22f9d54709fb4c47f09e371fef3d528
kcafernandes <kcafernandes06@gmail.com>
2026-08-18 18:50:22 -0700

    scaffold vite frontend and add homepage skeleton

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+      <section id="hero">
+        <div>
+          <h1>R' You Ready for Registration?</h1>
+          <p>
+          Scope out UCR courses, professors, prerequisites, and student insights—all in one place.
+          </p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
