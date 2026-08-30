070ce48c154087569627e67c0ac8011a50fc2a79
kcafernandes <kcafernandes06@gmail.com>
2026-08-29 21:47:02 -0700

    fixed search bar layout and added style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,13 +13,7 @@ function App() {
           <p>
           Scope out UCR courses, professors, prerequisites, and student insights—all in one place.
           </p>
-          <div className="search-wrap">
-            <svg className="search-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-              <circle cx="11" cy="11" r="8" />
-              <line x1="21" y1="21" x2="16.65" y2="16.65" />
-            </svg>
-            <SearchBar/>
-          </div>
+          <SearchBar/>
         </div>
       </section>
     </>
