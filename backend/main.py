05814d7e7c6fb35faddfbc48545556462e20dab7
Kesia Fernandes <74519872+kcafernandes@users.noreply.github.com>
2026-09-02 16:38:32 -0700

    add /course endpoint w/ mock data (#18)
    Closes #14

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+mock_courses = [
+    {"code": "MATH 009A", "title": "First-Year Calculus"},
+    {"code": "MATH 031", "title": "Linear Algebra"},
+    {"code": "MATH 046", "title": "Differential Equations"},
+]
+
 @app.get("/")
 def read_root():
     return {"message" : "scotty scope backend is running"}
+
+@app.get("/courses")
+def get_courses():
+    return mock_courses
