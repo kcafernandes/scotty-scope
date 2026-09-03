f6c2578a66fbbb3c7f02329ed33677ad675ce3e7
Kesia Fernandes <74519872+kcafernandes@users.noreply.github.com>
2026-09-02 17:26:07 -0700

    connected results page to backend API (#21)

--- frontend/src/components/ResultsPage.jsx
+++ frontend/src/components/ResultsPage.jsx
@@ -1,28 +1,28 @@
 import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import './ResultsPage.css'
 
 function ResultsPage(){
     const { subject }= useParams()
-    // make a list of objects that include course code + name of the class
-    // ex: CS 111 - Discrete Structures
+    const [courses, setCourses] = useState([])
 
-    const mockCourses = [
-        {code: 'MATH 009A', title: 'First-Year Calculus'},
-        {code: 'MATH 031', title: 'Linear Algebra'},
-        {code: 'MATH 046', title: 'Differential Equations'},
-    ]
+    useEffect(()=>{
+        fetch('http://127.0.0.1:8000/courses')
+            .then((response) => response.json())
+            .then((data) => setCourses(data))
+    }, [])
 
     return(
         <section className="results-page">
             <h2>Results for {subject}</h2>
 
-            {mockCourses.length === 0 && (
+            {courses.length === 0 && (
                 <p>No courses found :(</p>
             )}
 
-            {mockCourses.length > 0 && (
+            {courses.length > 0 && (
                 <ul className="course-list">
-                {mockCourses.map((course) => (
+                {courses.map((course) => (
                   <li key={course.code} className="course-card">
                     <span className="course-code">{course.code}</span>
                     <span className="course-title">{course.title}</span>
